eb14d9413fe7b3dade2a004cb45442b0fdf63618
bit <bit@bit-PC>
2015-09-22 16:09:56 +0900

    데이터 베이스 연결 초기 단계 1. 데이터 베이스 생성 2. mysql 명령어 입력 성공 3. 현재 위도, 경도 읽어오기 성공 4. 데이터 베이스 입력 테스트 중

--- src/main/webapp/js/app/result_json.js
+++ src/main/webapp/js/app/result_json.js
@@ -5,8 +5,30 @@ define(['jquery', 'handlebars', 'app/common'], function ($, handlebars) {
       console.log("->result_json->init()");
 
       $(document).ready(function () {
+          $.getJSON(contextRoot + "/json/company/list.do", function (data) {
+
+      	  console.log(data);
+          	
+            var source = $('#template1').html();
+            //          console.log(source);
+            var template = handlebars.compile(source);
+            //          console.log(template);
+
+            var content = template(data);
+
+            //          console.log("content : " + content);
+
+            $('#resultJson').html(content);
+
+          });
+        });
+      
+      
+/*      $(document).ready(function () {
         $.getJSON(contextRoot + "/json/test.json", function (data) {
 
+    	  console.log(data);
+        	
           var source = $('#template1').html();
           //          console.log(source);
           var template = handlebars.compile(source);
@@ -19,7 +41,7 @@ define(['jquery', 'handlebars', 'app/common'], function ($, handlebars) {
           $('#resultJson').html(content);
 
         });
-      });
+      });*/
 
 
     }
